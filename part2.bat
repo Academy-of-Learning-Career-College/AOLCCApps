fe3ddee81038716cd9ea6ffee70ce0c1d9c1f47f
Michael Ross <tech@o.aolccbc.com>
2020-03-17 13:27:17 -0700

    modified:   database.txt 	modified:   part2.bat

--- part2.bat
+++ part2.bat
@@ -1,1 +1,3 @@
- 
+echo installing typing trainer
+"%~dp0\typingtrainersetup_v1.68.exe" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+copy "%~dp0\database.txt" "c:\program files (x86)\TypingTrainer\database.txt"
