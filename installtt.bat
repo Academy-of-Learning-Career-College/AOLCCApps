012905a06b5ea9f0d39ef893e3256280f522a996
Michael Ross <tech@o.aolccbc.com>
2020-03-17 13:28:40 -0700

    modified:   installtt.bat

--- installtt.bat
+++ installtt.bat
@@ -2,5 +2,5 @@ echo Installing Pre-Requisits
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install -y vcredist-all git
 echo Downloading Latest Files
-%ProgramFiles%\Git\cmd\git.exe clone https://github.com/fireball8931/AOLCCApps %temp%\git
-%temp%\git\part2.bat
+%ProgramFiles%\Git\cmd\git.exe clone https://github.com/fireball8931/AOLCCApps c:\Typing\
+c:\Typing\part2.bat
