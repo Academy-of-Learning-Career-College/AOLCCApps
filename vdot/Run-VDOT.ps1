07f85af7af00ac29ee4981574003c582a3f7dd7d
Michael Ross <mike@aolccbc.com>
2023-12-04 16:00:25 -0800

    yuck

--- vdot/Run-VDOT.ps1
+++ vdot/Run-VDOT.ps1
@@ -28,9 +28,9 @@ try {
     $VDOTPath = Join-Path (Get-ChildItem -Path $targetDir -Directory).FullName 'Windows_VDOT.ps1'
     if(Test-Path $VDOTPath){
         # Validate script integrity and execute securely
-        $scriptContent = Get-Content -Path $VDOTPath -Raw
+        # $scriptContent = Get-Content -Path $VDOTPath -Raw
         if((Get-FileHash -Path $VDOTPath -Algorithm SHA256).Hash -eq "C1C4F959515B0CE55019D253EC4B881D32CB0959D260F6FDBF2A2163CD20028E"){
-            Invoke-Expression $scriptContent -AcceptEula -Verbose -Optimizations Autologgers, DefaultUserSettings, DiskCleanup, NetworkOptimizations, ScheduledTasks, Services
+            Invoke-Expression $VDOTPath -AcceptEula -Verbose -Optimizations Autologgers, DefaultUserSettings, DiskCleanup, NetworkOptimizations, ScheduledTasks, Services
         }
         else{
             Write-Host "VDOT script integrity check failed. Aborting execution."
