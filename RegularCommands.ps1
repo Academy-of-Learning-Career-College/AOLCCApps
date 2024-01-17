9b7931e1203761d8ef0e6dcf39d59390b5903069
Mike <mike@aolccbc.com>
2024-01-17 15:46:03 -0800

    Update RegularCommands.ps1

--- RegularCommands.ps1
+++ RegularCommands.ps1
@@ -62,6 +62,21 @@ if ($campus -ne 'OffSite') {
 	Invoke-WebRequest -Uri $databasesrc -OutFile $typingtrainerfolder\database.txt -Verbose -UseBasicParsing
 	Invoke-WebRequest -Uri $typingbatsrc -OutFile $typingbatdest -Verbose -UseBasicParsing
 
+ 	#create shortcuts for typing trainer
+  	$sm = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu'
+$urlFile = Join-Path $sm 'Connect to TypingTrainer.url'
+$tt = Join-Path ${ENV:ProgramFiles(x86)} 'TypingTrainer\typingtrainer.exe'
+$sf = Join-Path $env:HOMEDRIVE 'scriptfiles\typingtrainer.bat'
+$batURI = "URL=file://" + $sf.Replace('\','/')
+$icoFile = "IconFile=" +  $tt
+
+
+Set-Content -Path $urlFile -Value '[InternetShortcut]'
+Add-Content -Path $urlFile -Value $batURI
+Add-Content -Path $urlFile -Value $icoFile
+Add-Content -Path $urlFile -Value "IconIndex=0"
+Copy-Item -Path $urlFile -Destination (Join-Path $env:ALLUSERSPROFILE Desktop)
+
 	#download the client installer to C:\fogtemp\fog.msi
 	#$fogmsi = $scriptingdir + "fog.msi"
 	#Invoke-WebRequest -URI "http://fogserver./fog/client/download.php?newclient" -UseBasicParsing -OutFile $fogmsi
