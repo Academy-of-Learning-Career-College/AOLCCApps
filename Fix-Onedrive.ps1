4f29767387e1ae2067f316c2a79cefa243d3f7b4
Mike <mike@aolccbc.com>
2024-01-25 10:09:02 -0800

    Update Fix-Onedrive.ps1

--- Fix-Onedrive.ps1
+++ Fix-Onedrive.ps1
@@ -1,2 +1,3 @@
+Write-Host "Setting Policies to Enable Onedrive"
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSync' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
